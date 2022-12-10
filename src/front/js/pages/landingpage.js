2f22c862129da0ee3888d0cf08b26cd39bb5a68d
pablotori98 <pablotori98@gmail.com>
2022-12-10 19:25:41 +0000

    jumbotron de la landing page

---
+++ src/front/js/pages/landingpage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../styles/home.css";
+
+// Import components
+import { LandingJumbo } from "../component/jumbotron/landingjumbo";
+
+export const LandingPage = () =>{
+
+
+    return(
+        <div>
+            <LandingJumbo />
+        </div>
+        )
+}
