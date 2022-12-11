3444878e48736bda155fbc84769c2a681a17668e
pablotori98 <pablotori98@gmail.com>
2022-12-11 11:54:55 +0000

    Incorporación usestate a asidelandingpgregister.js

--- src/front/js/component/asides/AsideLandingPgRegister.js
+++ src/front/js/component/asides/AsideLandingPgRegister.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const AsideLandingPgRegister = () =>{
-
+    const [name, setName] = useState("")
+    const [surname, setSurname] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
     return (
         <div className="formAsideLp text-white my-5 mx-2">
         <div>
@@ -12,15 +15,15 @@ export const AsideLandingPgRegister = () =>{
         <form>
             <div className="m-3"><strong>Registrate y obten una cuenta gratuita</strong></div>
             <div className="m-3 d-flex">
-                <input type="text" className="form-control me-2" id="firstName" aria-describedby="firstname" placeholder="Nombre"/>
-                <input type="text" className="form-control ms-2" id="lastName" aria-describedby="lastname" placeholder="Apellidos"/>
+                <input type="text" className="form-control me-2" id="firstName" aria-describedby="firstname" placeholder="Nombre" onChange={(event)=> setName(event.target.value)}/>
+                <input type="text" className="form-control ms-2" id="lastName" aria-describedby="lastname" placeholder="Apellidos" onChange={(event)=> setSurname(event.target.value)}/>
 
             </div>
             <div className="m-3">
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"/>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" onChange={(event)=> setEmail(event.target.value)}/>
             </div>
             <div className="m-3">
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" onChange={(event)=> setPassword(event.target.value)}/>
             </div>
             </form>
             <div className="d-flex justify-content-center">
