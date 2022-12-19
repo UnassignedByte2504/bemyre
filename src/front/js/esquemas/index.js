c2c517385a366b2bcfeb740574e7456f10735586
pablotori98 <pablotori98@gmail.com>
2022-12-19 20:28:12 +0000

    Formik y yup 0.1

---
+++ src/front/js/esquemas/index.js
@@ -0,0 +1,34 @@
+import * as Yup from "yup";
+// paso 1 importar yup
+
+const emailRules =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const passwordRules =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+// Añadimos reglas
+
+// Hacemos el primer esquema
+
+export const signupSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Correo invalido")
+    .required("Correo obligatorio")
+    .matches(emailRules, "Correo no cumple los requisitos"),
+  confirmEmail: Yup.string()
+    .email("Correo invalido")
+    .required("Correo obligatorio")
+    .oneOf([Yup.ref("email"), null], "Correo no coincide"),
+  password: Yup.string()
+    .min(8, "No cumple caracteres minimos")
+    .required("Password necesaria")
+    .matches(passwordRules, "Password no cumple con los requisitos"),
+  confirmPassword: Yup.string()
+    .required("Confirmación contraseña obligatoria")
+    .oneOf([Yup.ref("password"), null], "Password no coincide"),
+  firstName: Yup.string().required("Nombre obligatorio"),
+  lastName: Yup.string().required("Apellido obligatorio"),
+  userName: Yup.string().required("Username necesario"),
+  confirmUserName: Yup.string()
+    .oneOf([Yup.ref("userName"), null], "UserName no coincide")
+    .required("Confirmación necesaria"),
+});
