06910dce1cb59ec29bb7298fae2ad86d61299137
pablotori98 <pablotori98@gmail.com>
2022-12-21 11:16:43 +0000

    Cambio a margin minus

--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -9,30 +9,22 @@ export const Profile = () =>{
 
     return(
         <>
-        {/* Inicio header */}
         <div className="position-relative position-relative-example headerProfile">
-            <div className=" d-flex flex-column align-items-end justify-content-center my-3 container nameHeader" >
+            <div className=" d-flex flex-column justify-content-center align-items-end container nameheader" >
                     <div><h1>Robert Russell</h1></div>
                     <div><p>Localidad: Sevilla</p></div>
             </div>
-            <div className="cardprofile"><CardProfile  /></div>
-
-            <div className="reproductor container">
-                <div><iframe style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/track/5rhMc6IqSdVsyF7bRieSTc?utm_source=generator" width="100%" height="152"  allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></div>
-                <div><iframe style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/track/6dFn6my1sHK2bcf23GlHwM?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></div>
-            </div>
         </div>
 
-        {/* Mobile */}
-        <div className=" container d-flex justify-content-center flex-wrap">
-            <div className="cardprofilemobile"><CardProfile  /></div>
-
-            <div className="reproductormobile container">
-                <div><iframe style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/track/5rhMc6IqSdVsyF7bRieSTc?utm_source=generator" width="100%" height="152"  allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></div>
-                <div><iframe style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/track/6dFn6my1sHK2bcf23GlHwM?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></div>
+        <div className="cardandreproductor container ">
+            <div className="cardprofile"><CardProfile  /></div>
+            <div className="reproductor mt-5">
+                    <div><iframe style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/track/5rhMc6IqSdVsyF7bRieSTc?utm_source=generator" width="100%" height="152"  allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></div>
+                    <div><iframe style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/track/6dFn6my1sHK2bcf23GlHwM?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></div>
             </div>
-                  
-        </div> 
+        </div>
+        
+
         </>
     )
 }
