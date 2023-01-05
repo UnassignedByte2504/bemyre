812a006cdba5563522af9413506333fdfa5c1710
UnassignedByte2504 <marcos.asp989@gmail.com>
2023-01-05 13:29:50 +0000

    SettingHeader component created @ usersettings/aux

--- src/front/js/component/usersettings/userpictures/UserPictures.js
+++ src/front/js/component/usersettings/userpictures/UserPictures.js
@@ -1,29 +1,49 @@
-import React from 'react'
-import {useState, useContext, useMemo, useEffect} from 'react'
-import {useParams} from 'react-router-dom'
-import { Context } from '../../../store/appContext'
-
+import React from "react";
+import { useState, useContext, useMemo, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Context } from "../../../store/appContext";
+//MUI imports >>>
+import {
+  Box,
+  Typography,
+  Button,
+  Grid,
+  Paper,
+  TextField,
+  InputAdornment,
+  IconButton,
+  Circular,
+  Slider
+} from "@mui/material";
+//MUI imports <<<
+import SettingHeader from "../aux/SettingHeader.jsx";
 const UserPictures = () => {
-  const { store, actions } = useContext(Context)
-  const params = useParams()
-  const profilePicture = store.resultados.profile_img ? store.resultados.profile_img : null;
-  const portraitPicture = store.resultados.portrait_img ? store.resultados.portrait_img : null;
-  const current_user = sessionStorage.getItem('current_user') ? sessionStorage.getItem('current_user') : false;
-  const userparams = params.username
+  const { store, actions } = useContext(Context);
+  const params = useParams();
+  const profilePicture = store.resultados.profile_img
+    ? store.resultados.profile_img
+    : null;
+  const portraitPicture = store.resultados.portrait_img
+    ? store.resultados.portrait_img
+    : null;
+  const current_user = sessionStorage.getItem("current_user")
+    ? sessionStorage.getItem("current_user")
+    : false;
+  const userparams = params.username;
 
-  useEffect(() =>{
-
-if(current_user !== userparams) {
-      window.location.href = `/user/${current_user}`}
-  }, [])
-
+  useEffect(() => {
+    if (current_user !== userparams) {
+      window.location.href = `/user/${current_user}`;
+    }
+  }, []);
 
   return (
-    <div>
+    <Box>
+      <SettingHeader title="Cambiar Imagen de Perfil y Portada"/>
       <span>{current_user}</span>
       <span>{userparams}</span>
-    </div>
-  )
-}
+    </Box>
+  );
+};
 
-export default UserPictures
+export default UserPictures;
